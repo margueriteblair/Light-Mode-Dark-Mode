2c641e9a585bffe3b561504e0496a9b11f8ff83b
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-25 19:55:28 -0400

    toggled between light and darkmode

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { ThemeProvider} from 'styled-components'
 import {lightTheme, darkTheme} from './theme'
-import { globalStyles } from '.global'
+import { GlobalStyles } from './global'
 
 function App() {
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }
   return (
     
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
 <>
-    <button>Toggle Theme</button>
-    <h1>It's light theme, bitch!</h1>
+    <GlobalStyles />
+    <button onClick={toggleTheme}>Toggle Theme</button>
+    <h1>{theme === 'light' ? "It's light theme, bitch!" : "Welcome to the darkside."}</h1>
     <footer>
     </footer>
 </>
