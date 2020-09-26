d067c76d14ee3941138389d81fe42080bdedef59
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-25 20:19:57 -0400

    added login and register buttons to app js and centered them

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { ThemeProvider} from 'styled-components'
 import {lightTheme, darkTheme} from './theme'
 import { GlobalStyles } from './global'
+import Button from './Button'
 
 function App() {
   const [theme, setTheme] = useState('light')
@@ -18,9 +19,13 @@ function App() {
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
 <>
     <GlobalStyles />
-    <button onClick={toggleTheme}>Toggle Theme</button>
-    <h1>{theme === 'light' ? "It's light theme, bitch!" : "Welcome to the darkside."}</h1>
-  <h3>{theme === 'light' ? 'Please choose an option below' : 'Oh, and welcome to our application.'}</h3>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <h1>{theme === 'light' ? "It's light theme, bitch!" : "Welcome to the darkside."}</h1>
+        <h3>{theme === 'light' ? 'Please choose an option below:' : 'Oh, and welcome to our application:'}</h3>
+        <div className="homepageBtns" style={{justifyContent: "space-evenly", textAlign: "center", flexDirection: "row", display: "flex"}}>
+        <Button text='Login'></Button>
+        <Button text="Register" ></Button>
+  </div>
     <footer>
     </footer>
 </>
