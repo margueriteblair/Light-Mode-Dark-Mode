ef03bbc83e2ea152156a0f611fa81b9ea824da29
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-25 20:09:36 -0400

    created Components folder and created button component

--- src/Components/App.js
+++ src/Components/App.js
@@ -20,6 +20,7 @@ function App() {
     <GlobalStyles />
     <button onClick={toggleTheme}>Toggle Theme</button>
     <h1>{theme === 'light' ? "It's light theme, bitch!" : "Welcome to the darkside."}</h1>
+  <h3>{theme === 'light' ? 'Please choose an option below' : 'Oh, and welcome to our application.'}</h3>
     <footer>
     </footer>
 </>
