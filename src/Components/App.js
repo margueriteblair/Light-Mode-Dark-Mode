f5b348180bbadeceab93ff81fb9ec51cba5384b9
Marguerite Blair <marguerite.blair@gmail.com>
2020-09-27 22:37:06 -0400

    got light and dark theme running

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,24 +2,35 @@ import React, {useState} from 'react';
 import { ThemeProvider} from 'styled-components'
 import {lightTheme, darkTheme} from './theme'
 import { GlobalStyles } from './global'
+import Button from './Button'
 import {BrowserRouter} from 'react-router-dom'
 import AppRouter from './AppRouter'
 
 function App() {
   const [theme, setTheme] = useState('light')
 
+  const toggleTheme = () => {
+    if (theme === 'light') {
+      setTheme('dark')
+      console.log('Theme is dark')
+    } else {
+      setTheme('light')
+      console.log('Theme is light')
+    }
+  }
 
   const loginLink = '/login';
   const regLink = '/register';
   return (
 <BrowserRouter>
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
-<>
     <GlobalStyles />
+    <button onClick={toggleTheme}>Toggle Theme</button>
+        <h1>{theme === 'light' ? "It's light theme, bitch!" : "Welcome to the darkside."}</h1>
+        <h3>{theme === 'light' ? 'Please choose an option below:' : 'Oh, and welcome to our application:'}</h3>  
     <AppRouter/>
     <footer>
     </footer>
-</>
     </ThemeProvider>
     </BrowserRouter>
   );
