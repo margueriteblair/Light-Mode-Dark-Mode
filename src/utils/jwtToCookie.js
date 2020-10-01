dfd57e5c6d1bbe245944daebabbf1762ac09718f
Marguerite Blair <marguerite.blair@gmail.com>
2020-10-01 14:56:46 -0400

    pseudocoding jwt to cookie util

---
+++ src/utils/jwtToCookie.js
@@ -0,0 +1,7 @@
+//with a static method, it's to be called on a class as a whole, not just an instance of the class
+//loool that's why it looks like java silly!!
+
+export default class Cookies {
+    //add static methods for cookies here
+    
+}
